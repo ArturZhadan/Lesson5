09330aa1e4b189fedb8e484dcf41d09fdc813be6
ArturZhadan <artur.zhadan.17@gmail.com>
2022-02-08 23:54:21 +0200

    First commit

---
+++ src/com/home/Main.java
@@ -0,0 +1,11 @@
+package com.home;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int [] array = {1,2,3,4,7,15};
+        System.out.println(Arrays.toString(array));
+    }
+}
